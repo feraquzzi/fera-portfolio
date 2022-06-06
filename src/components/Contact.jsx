bc13ebbf4bc0c9af686908db1587d643f382e988
Femi Olaleye <mosesobembe@gmail.com>
2022-06-06 22:20:44 +0100

    Worked with the UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './styles/contact.css';
+import { Telephone } from 'react-bootstrap-icons';
 
 const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact Us</h1>
+      <form>
+        <div className="contactform">
+          
+          <input type="text" className='name' placeholder='Full name' required/>
+          <br />
+          <div className='one'>
+            <input type="email" className='nametwo' placeholder='Email address' required/>
+            <input type="text" className='nametwo' placeholder='Phone number' required/>
+          </div>
+          <br />
+          <textarea className='name spec' placeholder='Message' required></textarea><br />
+          <div className='two'>
+            <button>Send</button>
+            <a href="tel:+234-703-693-2051" ><Telephone size={50} color='#10f7d8'/></a>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,39 @@ import './styles/services.css';
 const Projects = () => {
   return (
     <div className="services">
-      <h1>Services</h1>
+      <h1>My Job</h1>
+      <div className='allservice'>
+        <div className='service1'>
+          <div className="service1a">
+
+          </div>
+
+          <div className="service1b">
+            
+          </div>
+        </div>
+
+        <div className='service2'>
+          <div className="service2a">
+            <img src="shopify.png" alt='shopify'/>
+          </div>
+
+          <div className="service2b">
+            
+          </div>
+        </div>
+
+        <div className='service3'>
+          <div className="service3a">
+
+          </div>
+
+          <div className="service3b">
+  
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
